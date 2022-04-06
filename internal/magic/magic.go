bb92794c3bf216b4538d9ce0de670056b8675c7a
Mark <markh@sweepbright.com>
2022-04-06 21:59:54 +0300

    fix

--- internal/magic/magic.go
+++ internal/magic/magic.go
@@ -20,6 +20,19 @@ type MagicMap[V any] struct {
 	Value map[string]V
 }
 
+func NewMagicMap[V any]() *MagicMap[V] {
+	return &MagicMap[V]{Value: nil}
+}
+
+func (m *MagicMap[V]) Set(key string, value V) *MagicMap[V] {
+	if m.Value == nil {
+		m.Value = make(map[string]V)
+	}
+
+	m.Value[key] = value
+	return m
+}
+
 func (m *MagicMap[V]) UnmarshalJSON(data []byte) error {
 	var value map[string]V
 	err := json.Unmarshal(data, &value)
--- internal/magic/magic_test.go
+++ internal/magic/magic_test.go
@@ -1,6 +1,7 @@
 package magic
 
 import (
+	"encoding/json"
 	"reflect"
 	"testing"
 )
@@ -27,7 +28,7 @@ func TestMagic_UnmarshalJSON(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			m := &Magic{}
-			if err := m.UnmarshalJSON(tt.args.data); (err != nil) != tt.wantErr {
+			if err := json.Unmarshal(tt.args.data, m); (err != nil) != tt.wantErr {
 				t.Errorf("Magic.UnmarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if m.Value != tt.expected {
@@ -49,14 +50,14 @@ func TestMagicBoolMap_UnmarshalJSON(t *testing.T) {
 	}{
 		{
 			name:     "UnmarshalJSON",
-			expected: &MagicMap[bool]{Value: map[string]bool{"a": true}},
+			expected: NewMagicMap[bool]().Set("a", true),
 			args: args{
 				data: []byte(`{"a":true}`),
 			},
 		},
 		{
 			name:     "UnmarshalJSON",
-			expected: &MagicMap[bool]{Value: nil},
+			expected: NewMagicMap[bool](),
 			args: args{
 				data: []byte(`[]`),
 			},
@@ -65,7 +66,76 @@ func TestMagicBoolMap_UnmarshalJSON(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			m := &MagicMap[bool]{}
-			if err := m.UnmarshalJSON(tt.args.data); (err != nil) != tt.wantErr {
+			if err := json.Unmarshal(tt.args.data, m); (err != nil) != tt.wantErr {
+				t.Errorf("MagicBoolMap.UnmarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(m, tt.expected) {
+				t.Errorf("MagicBoolMap.UnmarshalJSON() = %v, want %v", m, tt.expected)
+			}
+		})
+	}
+}
+
+func TestMagicBoolMap_UnmarshalJSON_2(t *testing.T) {
+	type Attributes struct {
+		Features MagicMap[MagicMap[bool]]
+	}
+
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name     string
+		expected Attributes
+		args     args
+		wantErr  bool
+	}{
+		{
+			name: "UnmarshalJSON",
+			expected: Attributes{
+				Features: *NewMagicMap[MagicMap[bool]]().
+					Set("a", *NewMagicMap[bool]().
+						Set("b", true)),
+			},
+			args: args{
+				data: []byte(`{
+					"features": {
+						"a": {
+							"b":true
+						}
+					}
+				}`),
+			},
+		},
+		{
+			name: "UnmarshalJSON: wrong map type",
+			expected: Attributes{
+				Features: *NewMagicMap[MagicMap[bool]]().
+					Set("a", *NewMagicMap[bool]()),
+			},
+			args: args{
+				data: []byte(`{
+					"features": {
+						"a": []
+					}
+				}`),
+			},
+		}, {
+			name: "UnmarshalJSON: wrong map parent type",
+			expected: Attributes{
+				Features: *NewMagicMap[MagicMap[bool]](),
+			},
+			args: args{
+				data: []byte(`{
+					"features": []
+				}`),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := Attributes{}
+			if err := json.Unmarshal(tt.args.data, &m); (err != nil) != tt.wantErr {
 				t.Errorf("MagicBoolMap.UnmarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if !reflect.DeepEqual(m, tt.expected) {
